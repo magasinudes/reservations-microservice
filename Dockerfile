fd07c7b613030837ab25352be09374d4b075c2dc
Justin2997 <justin.brlotte797@gmail.com>
2019-05-30 14:57:32 -0400

    Fix Spring and Docker

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,7 @@ WORKDIR /src
 COPY    . .
 RUN     ./gradlew test build
 
+EXPOSE 8080
+
 ENTRYPOINT ["java"]
 CMD ["-jar", "build/libs/reservations-0.1.0.jar"]
--- src/main/java/reservations/ReservationsController.java
+++ src/main/java/reservations/ReservationsController.java
@@ -2,18 +2,17 @@ package reservations;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class ReservationsController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    @RequestBody
     public String index() {
         return "<h1>Reservation index!<h1>";
     }
 
     @RequestMapping(value = "/health", method = RequestMethod.GET)
-    @RequestBody
     public String health() {
         return "ok";
     }
