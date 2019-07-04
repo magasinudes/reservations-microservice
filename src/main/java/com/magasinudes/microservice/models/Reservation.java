d3f2b98337056de0a3c2f1488440865445c55074
stlaurentc <carl.stlaurent@usherbrooke.ca>
2019-07-04 11:46:31 -0400

    Ajout réservation

--- src/main/java/com/magasinudes/microservice/models/Reservation.java
+++ src/main/java/com/magasinudes/microservice/models/Reservation.java
@@ -1,4 +1,103 @@
 package com.magasinudes.microservice.models;
 
-public class Reservation {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "reservations", schema = "public")
+public class Reservation extends AuditModel {
+	private static final long serialVersionUID = -2250728570718420163L;
+    
+	@Id
+    @GeneratedValue(generator = "reservation_generator")
+    @SequenceGenerator(
+            name = "reservation_generator",
+            sequenceName = "reservation_sequence",
+            initialValue = 1
+    )
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "start_at", nullable = false)
+    private Date startAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "end_at", nullable = false)
+    private Date endAt;
+
+    @Column(name = "note")
+    private String note;
+
+    @JoinColumn(name = "user_id", nullable = false)
+    private String userId;
+
+    @JoinColumn(name = "resource_id", nullable = false)
+    private String resourceId;
+
+    // ------------
+    // Associations
+    // ------------
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "reservation_status_id", nullable = false)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private ReservationStatus reservationStatus;
+
+    // ------------
+    // Helpers
+    // ------------
+
+    public Long getId() {
+        return id;
+    }
+
+    public Date getStartAt() {
+        return startAt;
+    }
+
+    public void setStartAt(Date startAt) {
+        this.startAt = startAt;
+    }
+
+    public Date getEndAt() {
+        return endAt;
+    }
+
+    public void setEndAt(Date endAt) {
+        this.endAt = endAt;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(String resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public ReservationStatus getReservationStatus() {
+        return reservationStatus;
+    }
+
+    public void setReservationStatus(ReservationStatus status) {
+        this.reservationStatus = status;
+    }
 }
