7146e01095d7511bf45642e0970be77c35daa7d5
stlaurentc <carl.stlaurent@usherbrooke.ca>
2019-07-10 22:11:34 -0400

    ajout crud réservation

--- src/main/java/com/magasinudes/microservice/controllers/ReservationsController.java
+++ src/main/java/com/magasinudes/microservice/controllers/ReservationsController.java
@@ -1,4 +1,78 @@
 package com.magasinudes.microservice.controllers;
 
+import com.magasinudes.microservice.exceptions.RecordNotFoundException;
+import com.magasinudes.microservice.models.Reservation;
+import com.magasinudes.microservice.repositories.ReservationRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
 public class ReservationsController {
+
+	@Autowired
+	private ReservationRepository reservationRepository;
+
+	@GetMapping("/reservations")
+	public Page<Reservation> index(@PathVariable Long reservationID, Pageable pageable) {
+		return reservationRepository.findByReservationId(reservationID, pageable);
+	}
+
+	@PostMapping("/reservations")
+	public Long create(@PathVariable Long reservationID, @Valid @RequestBody Reservation reservation) {
+		return reservationRepository.save(reservation).getId();
+	}
+
+	@GetMapping("/reservations/{reservationId}")
+	public Optional<Reservation> show(@PathVariable Long reservationID) {
+		if (!reservationRepository.existsById(reservationID)) {
+			throw new RecordNotFoundException("No reservation found with id " + reservationID);
+		}
+
+		return reservationRepository.findById(reservationID);
+	}
+
+	@PutMapping("/reservations/{reservationId}")
+	public Long update(@PathVariable Long reservationID, @Valid @RequestBody Reservation data) {
+		if (!reservationRepository.existsById(reservationID)) {
+			throw new RecordNotFoundException("No reservation found with id " + reservationID);
+		}
+
+		return reservationRepository.findById(reservationID).map(reservation -> {
+			if (data.getStartAt() != null) {
+				reservation.setStartAt(data.getStartAt());
+			}
+			if (data.getEndAt() != null) {
+				reservation.setEndAt(data.getEndAt());
+			}
+			if (data.getNote() != null) {
+				reservation.setNote(data.getNote());
+			}
+			if (data.getUserId() != null) {
+				reservation.setUserId(data.getUserId());
+			}
+			if (data.getResourceId() != null) {
+				reservation.setResourceId(data.getResourceId());
+			}
+			return reservationRepository.save(reservation).getId();
+		}).orElseThrow(() -> new RecordNotFoundException("No reservation found with id " + reservationID));
+	}
+
+	@DeleteMapping("/reservations/{reservationId}")
+	public ResponseEntity<?> delete(@PathVariable Long reservationID) {
+		if (!reservationRepository.existsById(reservationID)) {
+			throw new RecordNotFoundException("No reservation found with id " + reservationID);
+		}
+
+		return reservationRepository.findById(reservationID).map(reservation -> {
+			reservationRepository.delete(reservation);
+			return ResponseEntity.ok().build();
+		}).orElseThrow(() -> new RecordNotFoundException("No reservation found with id " + reservationID));
+	}
 }
--- src/main/java/com/magasinudes/microservice/repositories/ReservationRepository.java
+++ src/main/java/com/magasinudes/microservice/repositories/ReservationRepository.java
@@ -1,12 +1,16 @@
 package com.magasinudes.microservice.repositories;
 
-//import com.magasinudes.microservice.models.Reservation;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-//}
-public interface ReservationRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.magasinudes.microservice.models.Reservation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+	Page<Reservation> findByReservationId(Long reservationID, Pageable pageable);
 }
 
